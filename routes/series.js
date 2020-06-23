2ca58b715e83912d3ad528dbd474e01b86bfc189
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-06-23 12:57:23 -0300

    completed

--- routes/series.js
+++ routes/series.js
@@ -35,11 +35,22 @@ router.get("/:id", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
   const serie = await Serie.findOne({ _id: req.params.id });
-  console.log(serie);
   Object.keys(req.body).forEach((key) => {
-    serie[key] = req.body.key;
+    if (key === "comments") {
+      serie[key].push(req.body[key]);
+    } else {
+      serie[key] = req.body[key];
+    }
   });
-  console.log(serie);
+  try {
+    await serie.save();
+    res.send(serie);
+  } catch (e) {
+    res.send({
+      success: false,
+      errors: Object.keys(e.errors),
+    });
+  }
 });
 
 module.exports = router;
