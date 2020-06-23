6dd08bada751aa15d96ca25b002c4829816270c1
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-06-23 12:34:49 -0300

    models and routes

---
+++ routes/series.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+
+const Serie = require("../models/serie");
+
+router.get("/", async (req, res) => {
+  const series = await Serie.find({});
+  res.send(series);
+});
+
+router.post("/", async (req, res) => {
+  const serie = new Serie(req.body);
+  try {
+    await serie.save();
+    res.send(serie);
+  } catch (e) {
+    res.send({
+      success: false,
+      errors: Object.keys(e.errors),
+    });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  await Serie.remove({ _id: req.params.id });
+  res.send({
+    success: true,
+  });
+});
+
+router.get("/:id", async (req, res) => {
+  const serie = await Serie.findOne({ _id: req.params.id });
+  res.send(serie);
+});
+
+router.put("/:id", async (req, res) => {
+  const serie = await Serie.findOne({ _id: req.params.id });
+  console.log(serie);
+  Object.keys(req.body).forEach((key) => {
+    serie[key] = req.body.key;
+  });
+  console.log(serie);
+});
+
+module.exports = router;
